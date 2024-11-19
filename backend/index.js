0ae5b83abe4e38413dc45ce57a32e73dd8424338
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-19 21:11:09 +0530

    Add MongoDB connection setup using Mongoose

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookie from "cookie-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 dotenv.config({});
 
 const app = express();
@@ -20,5 +21,6 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
