58d49d22f8e02e66c2141096ce659fcad7832bdf
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-19 20:47:14 +0530

    update indes.js and package.json

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cookie from "cookie-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    orgin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
 
 const PORT = 3000;
 
