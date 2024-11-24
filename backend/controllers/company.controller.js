d8a93fab1ddadb98030a3f783fa4065916369964
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-24 20:11:10 +0530

    Create getCompanyById controller

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -46,3 +46,23 @@ export const getCompany = async (req,res) => {
         console.log(error);
     }
 }
+
+
+export const getCompanyById = async (req,res) => {
+    try {
+       const companyId = req.params.id;
+       const company = await Company.findById(companyId);
+       if(!company){
+        return res.status(404).json({
+            message:"Company not found.",
+            success:false
+        })
+       } 
+       return res.status(200).json({
+            company,
+            success:true
+       })
+    } catch (error) {
+        console.log(error);
+    }
+}
