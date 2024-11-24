e04fd15e2685de461e91dfe7710c5e3d79e7cfbc
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-24 20:59:11 +0530

    Update index.js and company.router.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import cookie from "cookie-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+
 dotenv.config({});
 
 const app = express();
@@ -24,6 +25,7 @@ const PORT = process.env.PORT || 3000;
 
 // api's
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
 
 
 app.listen(PORT,()=>{
