e76d0516a3229f06effe00634c0df6ceebc1b5c1
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-25 20:04:49 +0530

    Create getJobById controller

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -58,3 +58,22 @@ export const getAllJobs = async (req,res) => {
         console.log(error);
     }
 }
+
+
+export const getJobById = async (req,res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId);
+        if(!job){
+            return res.status(400).json({
+                message:"Job not found.",
+                success:false
+            })
+        };
+
+        return res.status(200).json({job, success:true});
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
