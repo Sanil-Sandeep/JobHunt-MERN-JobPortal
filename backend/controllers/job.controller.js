bb6ee809ca9c0174b15118def6c083aae33d5c93
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-25 19:56:30 +0530

    Create getAllJobs controller

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -32,3 +32,29 @@ export const postJob = async (req,res) => {
         console.log(error);
     }
 }
+
+
+export const getAllJobs = async (req,res) => {
+    try {
+        const keyword = req.query.keyword || "";
+        const query = {
+            $or:[
+                {title:{$regex:keyword, $option:"i"}},
+                {description:{$regex:keyword, $option:"i"}},
+            ]
+        };
+        const jobs = await Job.find(query);
+        if(!jobs){
+            return res.status(404).json({
+                message:"Jobs not found.",
+                success:false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
