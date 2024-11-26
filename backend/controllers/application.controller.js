28e775a9e53bef0db0bf8cfc6340fd99f2ec7a4b
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-26 18:24:07 +0530

    Update applyJob Controller (check id the user has already aplied for the job)

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -1,3 +1,5 @@
+import { Application } from "../models/application.model";
+
 export const applyJob = async (req, res) => {
   try {
     const userId = req.id;
@@ -6,8 +8,20 @@ export const applyJob = async (req, res) => {
       return res.status(400).json({
         message: "Job id is required.",
         success: false,
-      });
+      })
+    };
+
+    // check id the user has already aplied for the job
+    const existingApplication = await Application.findOne({job:jobId, applicant:userId});
+
+    if(existingApplication){
+        return res.status(400).json({
+            message:"You have already applied for this job.",
+            success:false,
+        });
     }
+
+    
   } catch (error) {
     console.log(error);
   }
