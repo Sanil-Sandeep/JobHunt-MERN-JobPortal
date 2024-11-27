b4a572e508ef93be6ff067db06737ff08c7a0809
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-27 20:04:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,33 @@
-
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
 import Navbar from './components/shared/Navbar'
+
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+])
 
 function App() {
 
