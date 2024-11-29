e19b615b90d1107692fb8d3ae11e6b05e0e70419
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-29 18:36:47 +0530

    Create LatestJobCards.jsx

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LatestJobCard from './LatestJobCard';
+import LatestJobCards from './LatestJobCards';
 
 const randomJobs = [1,2,3,4,5,6,7,8];
 
@@ -9,7 +9,7 @@ const LatestJobs = () => {
         <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top </span> Job Openings</h1>
         <div className='grid grid-cols-3 gap-4 my-5'>
             {
-                randomJobs.slice(0,6).map((item, index) => <LatestJobCard/>)
+                randomJobs.slice(0,6).map((item, index) => <LatestJobCards/>)
             }
         </div>
         
