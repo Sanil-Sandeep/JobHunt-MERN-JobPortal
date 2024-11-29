07efc4664ee7c8f8ea0bc4ff7f711fff8daab45e
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-29 19:01:42 +0530

    Update App.jsx and Navbar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import { Home } from './components/Home'
+import Jobs from './components/Jobs'
 
 
 const appRouter = createBrowserRouter([
