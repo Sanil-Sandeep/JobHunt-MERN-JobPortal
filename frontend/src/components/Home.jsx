d24fba0f079be049eb1988752cd3807310a3c638
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-29 18:05:49 +0530

    Update LatestJobs.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import { CategoryCarousel } from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
 
 export const Home = () => {
   return (
