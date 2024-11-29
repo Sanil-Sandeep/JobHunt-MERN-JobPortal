e3d1e7a2398dd3b72739f2c21c3829bfbe00a102
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-29 19:15:22 +0530

    Update FilterCard.jsx and Jobs.jsx

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import FilterCard from './FilterCard'
 
 const Jobs = () => {
   return (
     <div>
         <Navbar/>
+        {/* Filter Page */}
+        <FilterCard/>
+        {/* Job Card */}
     </div>
   )
 }
