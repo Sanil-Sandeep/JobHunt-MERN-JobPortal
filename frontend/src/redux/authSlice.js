eaaa567e19d64e72fdcbaaf1a96db3666a561e8b
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-11-30 09:53:07 +0530

    Update authSlice.jsx

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name:"auth",
     initialState:{
-        loading:false
+        loading:false,
+        user:null
     },
     reducers:{
         // actions
         setLoadding:(state, action) => {
             state.loading = action.payload;
+        },
+        setUser:(state, action) => {
+            state.user = action.payload;
         }
     }
 });
-export const {setLoadding} = authSlice.actions;
+export const {setLoadding, setUser} = authSlice.actions;
 export default authSlice.reducer;
