f4bc61398768211ee522d3bd9a0cd29fbe44bae5
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-02 09:27:04 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req,res) => {
     try{
@@ -110,7 +112,10 @@ export const logout = async (req,res) => {
 export const updateProfile = async (req,res) => {
     try {
         const {fullname, email, phoneNumber, bio, skills} = req.body;
+        
         const file = req.file;
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
         let skillsArray;
         if(skills){
@@ -134,7 +139,10 @@ export const updateProfile = async (req,res) => {
         if(bio) user.profile.bio = bio
         if(skills) user.profile.skills = skillsArray
 
-
+        if(cloudResponse){
+            user.profile.resume = cloudResponse.secure_url // save the cloudinary url
+            user.profile.resumeOriginalName = file.originalname // save the original file name
+        }
 
         await user.save();
 
