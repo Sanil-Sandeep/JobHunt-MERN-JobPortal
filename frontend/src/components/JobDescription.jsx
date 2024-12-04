97a9fb22401026e88061916a7dd66d127a1aeba3
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-04 19:49:37 +0530

    Update

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Badge } from './ui/badge'
 import { Button } from './ui/button'
 import { useParams } from 'react-router-dom';
-import useGetSingleJob from '@/hooks/useGetSingleJob';
+import axios from 'axios';
+import { JOB_API_END_POINT } from '@/utils/constant';
+import { setSingleJob } from '@/redux/jobSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const JobDescription = () => {
 
     const isApplied = true;
     const params = useParams();
     const jobId = params.id;
-    useGetSingleJob(jobId); // custom hook to get single job
+    const {singleJob} = useSelector(store=>store.job);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchSingleJob = async () => {
+            try {
+                const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setSingleJob(res.data.jobs));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSingleJob();
+      },[jobId, dispatch, user?._id]);
 
   return (
     <div className='max-w-7xl mx-auto my-10'>
         <div className='flex items-center justify-between'>
             <div>
-                <h1 className='font-bold text-xl'>Frontend Developer</h1>
+                <h1 className='font-bold text-xl'>{singleJob?.title}</h1>
                 <div className='flex items-center gap-2 mt-4'>
-                    <Badge className={'text-blue-700 font-bold'} variant="ghost">12 Positions</Badge>
-                    <Badge className={'text-[#F83002] font-bold'} variant="ghost">Part Time</Badge>
-                    <Badge className={'text-[#7209B7] font-bold'} variant="ghost">24LPA</Badge>
+                    <Badge className={'text-blue-700 font-bold'} variant="ghost">{singleJob?.position} Positions</Badge>
+                    <Badge className={'text-[#F83002] font-bold'} variant="ghost">{singleJob?.jobType}</Badge>
+                    <Badge className={'text-[#7209B7] font-bold'} variant="ghost">Rs {singleJob?.salary}</Badge>
                 </div>
             </div>
             <Button 
@@ -30,11 +48,11 @@ const JobDescription = () => {
         </div>
         <h1 className='border-b-2 border-b-gray-300 font-medium py-4'>Job Description</h1>
         <div className='my-4'>  
-            <h1 className='font-bold my-1'>Role: <span className='pl-4 font-normal text-gray-800'>Fontend Developer</span></h1>
-            <h1 className='font-bold my-1'>Location: <span className='pl-4 font-normal text-gray-800'>Colombo</span></h1>
-            <h1 className='font-bold my-1'>Description: <span className='pl-4 font-normal text-gray-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span></h1>
-            <h1 className='font-bold my-1'>Experience: <span className='pl-4 font-normal text-gray-800'>2 years</span></h1>
-            <h1 className='font-bold my-1'>Salary: <span className='pl-4 font-normal text-gray-800'>12 LPA</span></h1>
+            <h1 className='font-bold my-1'>Role: <span className='pl-4 font-normal text-gray-800'>{singleJob?.title}</span></h1>
+            <h1 className='font-bold my-1'>Location: <span className='pl-4 font-normal text-gray-800'>{singleJob?.location}</span></h1>
+            <h1 className='font-bold my-1'>Description: <span className='pl-4 font-normal text-gray-800'>{singleJob?.description}</span></h1>
+            <h1 className='font-bold my-1'>Experience: <span className='pl-4 font-normal text-gray-800'>{singleJob?.experience}years</span></h1>
+            <h1 className='font-bold my-1'>Salary: <span className='pl-4 font-normal text-gray-800'>Rs {singleJob?.salary}</span></h1>
             <h1 className='font-bold my-1'>Total: <span className='pl-4 font-normal text-gray-800'>4</span></h1>
             <h1 className='font-bold my-1'>Posted Date: <span className='pl-4 font-normal text-gray-800'>17-07-2024</span></h1>
         </div>
