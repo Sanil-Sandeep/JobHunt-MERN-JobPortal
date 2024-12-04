4ca586683ffedc3f4290f97e1bec62ae67698ab6
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-04 19:34:16 +0530

    Update JobDescription.jsx and useGetSingleJob.jsx

--- frontend/src/hooks/useGetSingleJob.jsx
+++ frontend/src/hooks/useGetSingleJob.jsx
@@ -4,12 +4,12 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-const useGetSingleJob = () => {
+const useGetSingleJob = (jobId) => {
   const dispatch = useDispatch();  
   useEffect(() => {
     const fetchSingleJob = async () => {
         try {
-            const res = await axios.get(`${JOB_API_END_POINT}/get`,{withCredentials:true});
+            const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`,{withCredentials:true});
             if(res.data.success){
                 dispatch(setAllJobs(res.data.jobs));
             }
