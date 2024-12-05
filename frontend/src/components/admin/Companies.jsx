fca85e8ddf45e38dbddd01c4857d70f825a66da1
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-05 19:56:36 +0530

    Update Companies.jsx

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../shared/Navbar";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 const Companies = () => {
   return (
-    <div>Companies</div>
-  )
-}
+    <div>
+      <Navbar />
+      <div className="max-w-6xl mx-auto my-10">
+        <div className="flex items-center justify-between ">
+            <Input 
+                className="w-fit" 
+                placeholder="Filter by name" 
+            />
+          <Button>New Company</Button>
+        </div>
+        <CompaniesTable/>
+      </div>
+    </div>
+  );
+};
 
-export default Companies
+export default Companies;
