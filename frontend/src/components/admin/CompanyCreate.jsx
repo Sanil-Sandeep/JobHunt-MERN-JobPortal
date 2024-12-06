deb92a6a0873e686b162632a5a477d775d6f5013
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-06 08:42:11 +0530

    Update CompanyCreate.jsx

--- frontend/src/components/admin/CompanyCreate.jsx
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -4,10 +4,31 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
+import { COMPANY_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
 
 const CompanyCreate = () => {
 
   const navigate = useNavigate();
+  const [companyName, setCompanyName] = useState();
+
+  const registerNewCompany = async () => {
+    try {
+      const res = await axios.post(`${COMPANY_API_END_POINT}/register`, {companyName}, {
+        headers:{
+          'Content-Type':'application/json'
+        },
+        withCredentials:true
+      });
+      if(res?.data?.success){
+        toast.success(res.data.message);
+        const companyId = res?.data?.company?._id;
+        navigate(`/admin/companies/${companyId}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div>
@@ -23,11 +44,12 @@ const CompanyCreate = () => {
           type="text"
           className="my-2"
           placeholder="JobHunt, Microsoft etc."
+          onChange={(e) => setCompanyName(e.target.value)}
         />
 
         <div className="flex items-center gap-2 my-10">
           <Button variant="outline" onClick={() => navigate("/admin/companies")}>Cancel</Button>
-          <Button>Continue</Button>
+          <Button onClick={registerNewCompany}>Continue</Button>
         </div>
       </div>
     </div>
