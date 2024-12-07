f82052ef626e6a67d38256722eb70039d910713d
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-07 20:04:32 +0530

    Update

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -16,17 +16,18 @@ import { useNavigate } from "react-router-dom";
 
 const AdminJobsTable = () => {
   const { companies, searchCompanyByText } = useSelector((store) => store.company);
-  const [filterCompany, setFilterCompany] = useState(companies);
+  const {allAdminJobs} = useSelector(store=>store.job);
+  const [filterJobs, setFilterJobs] = useState(allAdminJobs);
   const navigate = useNavigate();
 
   useEffect(()=>{
-      const filteredCompany = companies.length >= 0 && companies.filter((company)=>{
+      const filteredCompany = allAdminJobs.length >= 0 && allAdminJobs.filter((job)=>{
         if(!searchCompanyByText){
           return true
         };
         return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
       });
-      setFilterCompany(filteredCompany);
+      setFilterJobs(filteredCompany);
   },[companies,searchCompanyByText])
 
   return (
@@ -43,15 +44,16 @@ const AdminJobsTable = () => {
         </TableHeader>
         <TableBody>
         {
-                filterCompany?.map((company) => (
+                filterJobs?.map((job) => (
                   <tr>
-                    <TableCell>{company.name}</TableCell>
-                    <TableCell>{company.createdAt.split("T")[0]}</TableCell>
+                    <TableCell>{job?.company?.name}</TableCell>
+                    <TableCell>{job?.title}</TableCell>
+                    <TableCell>{job?.createdAt.split("T")[0]}</TableCell>
                     <TableCell className="text-right curser-pointer">
                       <Popover>
                         <PopoverTrigger><MoreHorizontal /></PopoverTrigger>
                         <PopoverContent className="w-32">
-                          <div onClick={()=> navigate(`/admin/companies/${company._id}`)} className="flex items-center gap-2 w-fit cursor-pointer">
+                          <div onClick={()=> navigate(`/admin/companies/${job._id}`)} className="flex items-center gap-2 w-fit cursor-pointer">
                             <Edit2 className="w-4" />
                             <span>Edit</span>
                           </div>
