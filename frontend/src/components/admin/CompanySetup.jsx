0c9cbac8e77a9e4d4ff560f49f21b4ec15736c51
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-07 18:45:49 +0530

    Update

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -9,9 +9,12 @@ import axios from "axios";
 import { COMPANY_API_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
 import { useSelector } from "react-redux";
+import useGetCompanyById from "@/hooks/useGetCompanyById";
 
 const CompanySetup = () => {
 
+    const params = useParams();
+    useGetCompanyById(params.id);
     const [input, setInput] = useState({
         name:"",
         description:"",
@@ -22,7 +25,6 @@ const CompanySetup = () => {
 
     const {singleCompany} = useSelector(store=>store.company);
     const [loading, setLoading] = useState(false);
-    const params = useParams();
     const navigate = useNavigate();
 
     const changeEventHandler = (e) => {
