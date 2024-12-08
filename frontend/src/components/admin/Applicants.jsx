d9fae27db49817c79de05e61ca6ff7675e32ac76
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-08 18:55:02 +0530

    Update Applicants.jsx

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from '../shared/Navbar'
+import ApplicantsTable from './ApplicantsTable'
 
 const Applicants = () => {
   return (
-    <div>Applicants</div>
+    <div>
+        <Navbar />
+        <div className='max-w-7xl mx-auto'>
+            <h1 className='font-bold text-xl my-5'>Applicants (3)</h1>
+            <ApplicantsTable/>
+        </div>
+    </div>
   )
 }
 
