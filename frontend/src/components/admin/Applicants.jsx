24731adf27543e3935256ad48eb7b97651f7ca8f
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-08 19:32:52 +0530

    Update Applicants.jsx

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../shared/Navbar'
 import ApplicantsTable from './ApplicantsTable'
+import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import axios from 'axios'
+import { APPLICATION_API_END_POINT } from '@/utils/constant'
+import { setAllApplicants } from '@/redux/applicationSlice'
 
 const Applicants = () => {
+
+    const params = useParams();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchAllApplicants = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/${params._id}/applicants`, {withCredentials: true});
+                if (res.data.success) {
+                    dispatch(setAllApplicants(res.data.job));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    })
+
   return (
     <div>
         <Navbar />
