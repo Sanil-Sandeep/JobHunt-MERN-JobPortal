11d1dd7ed34a4d1c5c3ea3f14016cea776297e60
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-12-10 19:58:38 +0530

    Update

--- frontend/src/components/admin/ProtectedRoute.jsx
+++ frontend/src/components/admin/ProtectedRoute.jsx
@@ -1,21 +1,22 @@
-const { useEffect } = require("react");
-const { useSelector } = require("react-redux");
-const { useNavigate } = require("react-router-dom");
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 const ProtectedRoute = ({children}) => {
     const {user} = useSelector(store=>store.auth);
 
-    const navitate = useNavigate();
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        if(user == null || user.role != 'recruiter'){
-            navitate("/");
+    useEffect(()=>{
+        if(user === null || user.role !== 'recruiter'){
+            navigate("/");
         }
     },[]);
 
     return (
         <>
-            {children}
+        {children}
         </>
     )
 };
